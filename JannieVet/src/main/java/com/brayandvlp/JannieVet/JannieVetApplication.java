a67e4fd7cb49f7bd0fc0fe58490e335a6baba56f
Artu <brayandiaz258f@gmail.com>
2025-01-20 16:12:48 -0500

    Cargando el proyecto. Cafesito con leche.

---
+++ JannieVet/src/main/java/com/brayandvlp/JannieVet/JannieVetApplication.java
@@ -0,0 +1,13 @@
+package com.brayandvlp.JannieVet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JannieVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JannieVetApplication.class, args);
+	}
+
+}
