f9d7725e54d986a8556772956279284ba6f91a61
Artu <brayandiaz258f@gmail.com>
2025-01-20 18:24:31 -0500

    Creación de atributos para entidad Cliente.

--- JannieVet/src/main/java/com/brayandvlp/JannieVet/domain/cliente/Cliente.java
+++ JannieVet/src/main/java/com/brayandvlp/JannieVet/domain/cliente/Cliente.java
@@ -1,4 +1,31 @@
 package com.brayandvlp.JannieVet.domain.cliente;
 
+import com.brayandvlp.JannieVet.domain.direccion.Direccion;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "clientes")
+@Entity(name = "Cliente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long documento;
+    private String nombreCompleto;
+    private Integer numeroTelefonico;
+    private String email;
+    @Embedded
+    private Direccion direccion;
+    private LocalDateTime fecha;
+    private Boolean activo;
 }
